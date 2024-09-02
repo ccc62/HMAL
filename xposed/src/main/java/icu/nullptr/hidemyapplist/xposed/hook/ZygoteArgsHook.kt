4497b5ee8e08188e8b979c98f117a3168b1aac27
ccc62 <2071674807@qq.com>
2024-09-02 19:07:11 +0800

    Initial commit

---
+++ xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/hook/ZygoteArgsHook.kt
@@ -0,0 +1,57 @@
+package icu.nullptr.hidemyapplist.xposed.hook
+
+import android.annotation.TargetApi
+import android.os.Build
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import de.robv.android.xposed.XC_MethodHook
+import icu.nullptr.hidemyapplist.common.CommonUtils
+import icu.nullptr.hidemyapplist.common.Constants.*
+import icu.nullptr.hidemyapplist.xposed.HMAService
+
+@TargetApi(Build.VERSION_CODES.S)
+class ZygoteArgsHook(private val service: HMAService) : IFrameworkHook {
+
+    companion object {
+        private const val TAG = "HMAL-ZAH"
+        private val sAppDataIsolationEnabled = CommonUtils.isAppDataIsolationEnabled
+        private val sVoldAppDataIsolationEnabled = CommonUtils.isVoldAppDataIsolationEnabled
+    }
+
+    private var hook: XC_MethodHook.Unhook? = null
+
+    override fun load() {
+        if (!service.config.forceMountData) return
+        hook = findMethod("android.os.ZygoteProcess") {
+            name == "startViaZygote"
+        }.hookBefore { param ->
+            runCatching {
+                val uid = param.args[2] as Int
+                if (uid == UID_SYSTEM) return@hookBefore
+                val apps = service.pms.getPackagesForUid(uid) ?: return@hookBefore
+                for (app in apps) {
+                    if (service.isHookEnabled(app)) {
+                        if (sAppDataIsolationEnabled) param.args[20] = true // boolean bindMountAppsData
+                        if (sVoldAppDataIsolationEnabled) param.args[21] = true // boolean bindMountAppStorageDirs
+                        return@hookBefore
+                    }
+                }
+            }.onFailure {
+                unload()
+            }
+        }
+    }
+
+    override fun unload() {
+        hook?.unhook()
+        hook = null
+    }
+
+    override fun onConfigChanged() {
+        if (service.config.forceMountData) {
+            if (hook == null) load()
+        } else {
+            if (hook != null) unload()
+        }
+    }
+}
